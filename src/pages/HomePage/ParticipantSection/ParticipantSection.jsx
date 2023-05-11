a7b17a09f1e5e6f0d89600d77eff489f81335a03
Vindiza Aziz <vindizaaziz@gmail.com>
2023-05-11 17:04:58 +0600

    add swiper json and add participant swiper

--- src/pages/HomePage/ParticipantSection/ParticipantSection.jsx
+++ src/pages/HomePage/ParticipantSection/ParticipantSection.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './ParticipantSection.sass'
-
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay } from "swiper";
 const ParticipantSection = () => {
     return (
         <section className="participant">
@@ -8,13 +12,38 @@ const ParticipantSection = () => {
                 Участники
             </h2>
             <div className="participant__container container_fluid">
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
-                <div className="participant__card"></div>
+
+
+                <Swiper
+                    loop={true}
+                    slidesPerView={6}
+                    spaceBetween={14}
+                    autoplay={{
+                        delay: 0,
+                        disableOnInteraction: false
+                    }}
+                    grabCursor={true}
+                    speed={2000}
+                    modules={[Autoplay]}
+                    className="Participant__Swiper"
+                >
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">1</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">2</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">3</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">4</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">5</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">6</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">7</div></SwiperSlide>
+                    <SwiperSlide><div className="participant__card">8</div></SwiperSlide>
+
+                </Swiper>
             </div>
         </section>
     );
