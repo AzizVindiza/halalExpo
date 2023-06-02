64ae38fef64fd40d10b5185883ab2217f8f946d6
Ihiya Sultan <ihiya.sultan@mail.ru>
2023-06-02 18:07:26 +0600

    NewsPage Btn

--- src/pages/NewsPage/CartNewsSection/CartNewsSection.jsx
+++ src/pages/NewsPage/CartNewsSection/CartNewsSection.jsx
@@ -10,7 +10,7 @@ const CartNewsSection = () => {
             <h2 className="cartNewsSection__h2">12.10.2023</h2>
             <h3 className="cartNewsSection__h3">Lorem Ipsum is simply dummy </h3>
             <p className="cartNewsSection__p">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
-            <Btn text={'Читать далее'}/>
+            <Btn m={'card'} text={'Читать далее'}/>
 
         </div>
     );
