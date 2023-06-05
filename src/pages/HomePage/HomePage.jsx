daa316ac8018ac29042a2e9d1232030526c51a09
Aybekmamyshev <aibekm591@gmail.com>
2023-06-05 11:11:35 +0600

    homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ import AboutSection from "./AboutSection/AboutSection";
 import TargetSection from "./TargetSection/TargetSection";
 import TaskSection from "./TaskSection/TaskSection";
 import TypeSection from "./TypeSection/TypeSection";
-import SectorSection from "./SectorSection/SectorSection";
+
 import OpportunitySection from "./OpportunitySection/OpportunitySection";
 import MapSection from "./MapSection/MapSection";
 import SpeakersSection from "./SpeakersSection/SpeakersSection";
@@ -13,6 +13,8 @@ import OrganizatorSection from "./OrganizatorSection/OrganizatorSection";
 import VideoSection from "./VideoSection/VideoSection";
 import PartnersSection from "./PartnersSection/PartnersSection";
 import OfficeMapSection from "./OfficeMapSection/OfficeMapSection";
+import StartSection from "./StartSection/StartSection";
+import ZoneSection from "./ZoneSection/ZoneSection";
 
 const HomePage = () => {
     return (
@@ -26,7 +28,9 @@ const HomePage = () => {
             <TaskSection/>
             <TypeSection/>
             <VideoSection/>
-            <SectorSection/>
+            <StartSection/>
+            <ZoneSection/>
+
             <OpportunitySection/>
             <MapSection/>
             {/*<SpeakersSection/>*/}
--- src/pages/HomePage/StartSection/StartSection.jsx
+++ src/pages/HomePage/StartSection/StartSection.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
-
+import "./start.sass"
+import svg from "./1.svg"
+import svg2 from "./2.svg"
+import svg3 from "./3.svg"
+import svg4 from "./svg4.png"
+import svg5 from "./svg5.png"
+import svg6 from "./svg6.png"
 const StartSection = () => {
     return (
         <section className="start">
             <div className="container start__container">
-                <h2 className="start__h2">ИННОВАЦИИ НАЧИНАЕТСЯ ЗДЕСЬ</h2>
                 <div className="start__wrapper">
-                    <div className="start__bullet">
-                        <img src={} alt=""/>
+                    <h2 className="start__h2">ИННОВАЦИИ НАЧИНАЕТСЯ ЗДЕСЬ</h2>
+                    <div className="start__content">
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">trade zone</h3>
+                        </div>
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg2} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">fashion zone</h3>
+                        </div>
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg3} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">food zone</h3>
+                        </div>
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg4} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">invest zone</h3>
+                        </div>
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg5} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">national attribute zone</h3>
+                        </div>
+                        <div className="start__block">
+                            <div className="start__svg">
+                                <img src={svg6} alt="" className="start__img"/>
+                            </div>
+                            <h3 className="start__h3">halal zone</h3>
+                        </div>
+                    </div>
+                    <div className="wrapp">
+                        <h4 className="start__h4">Lorem Ipsum is simply dummy</h4>
+                        <p className="start__p">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                     </div>
                 </div>
+
 
             </div>
         </section>
