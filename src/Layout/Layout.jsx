b36f3669fc327141bc727089fe27c89c9a8587b9
Aziz Vindiza <vindizaaziz@gmail.com>
2023-06-13 14:11:02 +0600

    fix z index in first section

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import SmoothScroll from "../components/SmoothScroll/SmoothScroll";
 import {ScrollRestoration} from "react-router-dom";
 import DemoForm from "../components/DemoRegistraion/DemoForm/DemoForm";
 import Advertising from "./Advertising/Advertising";
