d56e7e7a5032117d55c2c93a937229210503531e
Ihiya Sultan <ihiya.sultan@mail.ru>
2023-06-23 17:44:09 +0600

    InvestorPage

--- src/pages/InvestorPage/Investor.jsx
+++ src/pages/InvestorPage/Investor.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import MapSection from "./MapSection/MapSection";
-import CountrySection from "./CountrySection/CountrySection";
 
 const Investor = () => {
     return (
         <>
             <MapSection/>
-            <CountrySection/>
+
         </>
     );
 };
