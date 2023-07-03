790b7a6e4f7d69e85e00905af2a80a79e95d1d62
Ihiya Sultan <ihiya.sultan@mail.ru>
2023-07-03 12:20:42 +0600

    HomePage

--- src/pages/HomePage/ZoneSection/ZoneCard/ZoneCard.jsx
+++ src/pages/HomePage/ZoneSection/ZoneCard/ZoneCard.jsx
@@ -3,7 +3,10 @@ import './zoneCard.sass'
 import ZoneCardSmall from "./ZoneCardSmall/ZoneCardSmall";
 import fashion from  "./fashionZone.png"
 import food from "./foodZone.png"
-import pic from './Rectangle 28.png'
+import trade from './tradeZone.png'
+import invest from './investZone.png'
+import National from './National atribute Zone.png'
+
 import {useTranslation} from "react-i18next";
 
 const ZoneCard = () => {
@@ -11,11 +14,11 @@ const ZoneCard = () => {
 
     return (
         <section className={"zoneCard"}>
-         <ZoneCardSmall item={t("zoneSection.tradeZone",{ returnObjects: true })} img={pic}/>
-         <ZoneCardSmall item={t("zoneSection.investZone",{ returnObjects: true })} img={pic}/>
+         <ZoneCardSmall item={t("zoneSection.tradeZone",{ returnObjects: true })} img={trade}/>
+         <ZoneCardSmall item={t("zoneSection.investZone",{ returnObjects: true })} img={invest}/>
          <ZoneCardSmall item={t("zoneSection.fashionZone",{ returnObjects: true })} img={fashion}/>
          <ZoneCardSmall item={t("zoneSection.foodZone",{ returnObjects: true })} img={food}/>
-         <ZoneCardSmall item={t("zoneSection.nationalZone",{ returnObjects: true })} img={pic}/>
+         <ZoneCardSmall item={t("zoneSection.nationalZone",{ returnObjects: true })} img={National}/>
         </section>
     );
 };
