de2d7e90530cfa4fa50593b8eab962f877684367
shikaridoki <rasul.vindiza@gmail.com>
2023-07-11 15:39:09 +0600

    mass section

--- src/pages/MassPage/MassSection/MassSection.jsx
+++ src/pages/MassPage/MassSection/MassSection.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import "./MassSection.sass"
 
+import camera from "./camera2.png"
+
+
 const MassSection = ({item}) => {
     return (
         <div className="massSection">
             <h2 className="massSection__h2">{item.title}</h2>
+            <div className="massSection__img">
+                <img src={camera} alt=""/>
+            </div>
+
             <div className="massSection__container container">
                 <p className={"massSection__p"}>Спасибо за ваш интерес к Halal Investment and Trade Expo 2023. Мы готовы предоставить вам возможность аккредитации на наше мероприятие, чтобы вы могли освещать его и сообщать о ключевых событиях и развитии халяльной индустрии. Пожалуйста, ознакомьтесь с правилами аккредитации и соблюдайте следующие требования:</p>
 
                 <div className="massSection__wrapper">
                     <ul className={"massSection__ul"}>
                         <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>Аккредитация СМИ осуществляется только до 20 июля. Просим вас отправить заявку на аккредитацию до этой даты, чтобы мы могли обработать ее своевременно. Подать заявку можно с помощью формы регистрации по
-                             <a href="">ссылке</a>>> </p></li>
-                        <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>Ваша
-                            заявка на аккредитацию должна содержать следующую информацию: полное название СМИ, ФИО представителя, контактные данные (телефон, электронная почта) и подробное описание целей и плана вашего освещения мероприятия.</p> </li>
+                              <a href=""> ссылке>></a> </p></li>
+                        <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>Ваша заявка на аккредитацию должна содержать следующую информацию: полное название СМИ, ФИО представителя, контактные данные (телефон, электронная почта) и подробное описание целей и плана вашего освещения мероприятия.</p> </li>
                         <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>Пожалуйста, предоставьте копию вашего журналистского удостоверения или другого документа, подтверждающего вашу профессиональную принадлежность к СМИ. </p></li>
                         <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>Аккредитация может быть предоставлена только журналистам, репортерам, фотографам и видеооператорам, работающим в аккредитованных СМИ. Приглашение на мероприятие не распространяется на сопровождающих лиц.</p></li>
                         <li className={"massSection__li"}><div className={"massSection__circle"}></div><p className={"massSection__item"}>При посещении мероприятия, пожалуйста, помните, что вы представляете свое СМИ. Следуйте этическим принципам и проявляйте уважение к участникам, остальным представителям СМИ и посетителям выставки</p></li>
@@ -22,6 +28,8 @@ const MassSection = ({item}) => {
 
                     </ul>
                     <p className="massSection__text">
+
+
                         Мы ценим ваше участие и поддержку в освещении Halal Investment and Trade Expo 2023. Просим вас соблюдать данные правила и быть аккуратными при осуществлении своей профессиональной деятельности во время мероприятия.
                     </p>
                 </div>
