b292d14bc996bcba084b8b2929c13c641d307068
Ihiya Sultan <ihiya.sultan@mail.ru>
2023-07-14 18:16:18 +0600

    PartnersPage

--- src/pages/PartnersPage/PartnersPage.jsx
+++ src/pages/PartnersPage/PartnersPage.jsx
@@ -3,12 +3,18 @@ import PartnerSection from "./PartnerSection/PartnerSection";
 import ApplicationSection from "./ApplicationSection/ApplicationSection";
 
 import PlusSection from "./PlusSection/PlusSection";
+import PartnersSection from "../HomePage/PartnersSection/PartnersSection";
+import PartnersOfficial from "../HomePage/PartnersOfficial/PartnersOfficial";
+import PartnersInfo from "../HomePage/PartnersInfo/ParnersInfo";
 
 
 const PartnersPage = () => {
     return (
         <>
             <PartnerSection/>
+            <PartnersSection/>
+            <PartnersOfficial/>
+            <PartnersInfo/>
             <ApplicationSection/>
             <PlusSection/>
 
