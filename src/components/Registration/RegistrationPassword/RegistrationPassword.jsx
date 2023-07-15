19a59c1eb75cf252f9f3df592e6c16040b6389fa
Aziz Vindiza <vindizaaziz@gmail.com>
2023-07-15 13:56:35 +0600

    update endpoint in registration and password key

--- src/components/Registration/RegistrationPassword/RegistrationPassword.jsx
+++ src/components/Registration/RegistrationPassword/RegistrationPassword.jsx
@@ -15,7 +15,7 @@ const RegistrationPassword = ({title , name} ) => {
             <label className="registration__label">
                 <h3 className="registration__label-title">Придумайте пароль<span className="registration__star">*</span> <span
                     className="registration__error"> {errors.password_user && errors.password_user.message}</span></h3>
-                <input className="registration__input" ref={password} type={passwordShown ? "text" : 'password'} {...register('password_user', {
+                <input className="registration__input" ref={password} type={passwordShown ? "text" : 'password'} {...register('password', {
                     required: {
                         message: "Это поле обязательно к заполнению!",
                         value: true
