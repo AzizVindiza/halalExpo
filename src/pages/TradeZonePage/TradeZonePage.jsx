f78454156de3046f28eae09314de6926dc369974
Aziz Vindiza <vindizaaziz@gmail.com>
2023-07-17 17:17:58 +0600

    update trade

--- src/pages/TradeZonePage/TradeZonePage.jsx
+++ src/pages/TradeZonePage/TradeZonePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import TradeFirst from "./TradeFirst/TradeFirst";
 import TradeSecond from "./TradeSecond/TradeSecond";
+import img from "./TRADE ZONE.png";
 
 const TradeZonePage = () => {
     return (
         <>
-            <TradeFirst/>
-            <TradeSecond/>
+            {/*<TradeFirst/>*/}
+            {/*<TradeSecond/>*/}
+            <div style={{width: "1440px"}}>
+                <img src={img} alt="fashion"/>
+            </div>
         </>
     );
 };
