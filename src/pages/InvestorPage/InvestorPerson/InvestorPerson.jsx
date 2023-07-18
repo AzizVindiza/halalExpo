ca533b514e37fbc2186656a415b39588be404b32
Aziz Vindiza <vindizaaziz@gmail.com>
2023-07-18 19:47:01 +0600

    add axios in members

---
+++ src/pages/InvestorPage/InvestorPerson/InvestorPerson.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InvestorPerson = () => {
+    return (
+        <div className={"investor-person"}>
+            
+        </div>
+    );
+};
+
+export default InvestorPerson;
