e0447f07312d78a4e30a2d0045e5a65e7d21d10d
Aybekmamyshev <aibekm591@gmail.com>
2023-07-21 21:02:13 +0600

    formZone next btn

--- src/pages/AccountPage/Ticket/FormZone/FormZone.jsx
+++ src/pages/AccountPage/Ticket/FormZone/FormZone.jsx
@@ -12,22 +12,27 @@ import {queryAllByAltText} from "@testing-library/react";
 const FormZone = () => {
 
 
-
-    const {setChooseZone,other,setOther,chooseZone,chooseIndustry,setChooseIndustry,setState} = useContext(CustomContext)
+    const {
+        setChooseZone,
+        other,
+        setOther,
+        chooseZone,
+        chooseIndustry,
+        setChooseIndustry,
+        setState
+    } = useContext(CustomContext)
 
 
     const clickOpenNextPage = () => { //Function let to go next page
-        if (chooseZone.length === 0 || chooseIndustry.length === 0)
-            return (
-                 setState("default")
-
-            )
+        if (chooseZone.length === 0 || chooseIndustry.length === 0){
+            setState("default")
+            alert("Заполните поля")
+        }
 
         else {
             setState("zone")
         }
     }
-
 
 
     const zona = ['Trade', 'Invest', 'Fashion', 'Food']
@@ -37,60 +42,60 @@ const FormZone = () => {
 
         <div className={'formZone'}>
 
-                <h2 className="formZone__h2">Пожалуйста выберите зону </h2>
-                <div className="formZone__first">
-                    {
-                        zona.map((item) => {
-                            return (
-                                <div key={item} className="formZone__zona">
-                                    <input  value={item} onChange={(e) => setChooseZone(e.target.value)}
-                                           type={"radio"} name={'range'} className="formZone__circle"/>
-                                    <h2 className="formZone__h3">{item}</h2>
-                                </div>
-                            )
-                        })
-                    }
-
-                </div>
-                <h2 className="formZone__h2 .h2">Пожалуйста выберите отрасль</h2>
-                <div className="formZone__second">
-                    {
-                        objectIndustry.map((item) => (
-                            <FormCheckZone key={item} item={item}/>
-                        ))
-                    }
-                    <div style={{width: "80px"}} onClick={() => setOther(!other) } className={'formZone__single'}>
-                        <input className="formZone__input" type="radio"/>
-                        <label  className="formZone__label formZone__label_second">
-                            <h2 className="formZone__h3 formZone__h3_second">Другое</h2>
-                        </label>
-
-                    </div>
-
-                </div>
-
+            <h2 className="formZone__h2">Пожалуйста выберите зону </h2>
+            <div className="formZone__first">
                 {
-                    other ?
-                        <div className={'formZone__industry'}>
-                            <h2 className="formZone__h2">Впишите деятельность, которую будете представлять</h2>
-                            <label className="formZone__border">
-                                <input
-                                    name={"radio"}
-                                    onChange={(event) => setChooseIndustry(event.target.value)}
-                                    type="text" className="formZone__text"/>
-                            </label>
-                        </div> : ""
-
+                    zona.map((item) => {
+                        return (
+                            <div key={item} className="formZone__zona">
+                                <input value={item} onChange={(e) => setChooseZone(e.target.value)}
+                                       type={"radio"} name={'range'} className="formZone__circle"/>
+                                <h2 className="formZone__h3">{item}</h2>
+                            </div>
+                        )
+                    })
                 }
 
-                <div className="formZone__last">
-                    <div></div>
-
-                    <div onClick={clickOpenNextPage}>
-                        <Btn type={"submit"} text={'Далее'}/>
-                    </div>
+            </div>
+            <h2 className="formZone__h2 .h2">Пожалуйста выберите отрасль</h2>
+            <div className="formZone__second">
+                {
+                    objectIndustry.map((item) => (
+                        <FormCheckZone key={item} item={item}/>
+                    ))
+                }
+                <div style={{width: "80px"}} onClick={() => setOther(!other)} className={'formZone__single'}>
+                    <input className="formZone__input" type="radio"/>
+                    <label className="formZone__label formZone__label_second">
+                        <h2 className="formZone__h3 formZone__h3_second">Другое</h2>
+                    </label>
 
                 </div>
+
+            </div>
+
+            {
+                other ?
+                    <div className={'formZone__industry'}>
+                        <h2 className="formZone__h2">Впишите деятельность, которую будете представлять</h2>
+                        <label className="formZone__border">
+                            <input
+                                name={"radio"}
+                                onChange={(event) => setChooseIndustry(event.target.value)}
+                                type="text" className="formZone__text"/>
+                        </label>
+                    </div> : ""
+
+            }
+
+            <div className="formZone__last">
+                <div></div>
+
+                <div className={`${chooseZone.length === 0 || chooseIndustry.length === 0 ? "formZone__next" : "formZone__next_active"} `}  onClick={clickOpenNextPage}>
+                    Далее
+                </div>
+
+            </div>
         </div>
 
 
